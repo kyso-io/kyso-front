8dfa7a05ca322320228d2b50281857f0ea3997e3
Fran Barrena <francisco@kyso.io>
2022-08-12 13:32:21 +0200

    fix: eslint

--- src/model/creation-report-file.ts
+++ src/model/creation-report-file.ts
@@ -1,11 +1,16 @@
 export class CreationReportFileSystemObject {
   public id: string;
+
   public path: string;
+
   public name: string;
+
   public type: string;
+
   public parentId: string | null;
+
   public text: string | null;
-  
+
   constructor(id: string, path: string, name: string, type: string, text: string, parentId?: string) {
     this.id = id;
     this.name = name;
@@ -13,7 +18,7 @@ export class CreationReportFileSystemObject {
     this.type = type;
     this.text = text;
 
-    if(parentId) {
+    if (parentId) {
       this.parentId = parentId;
     } else {
       this.parentId = null;
