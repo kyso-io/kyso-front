9681ec8ce3c6a6b3b96cfe1822ade01a3e838494
Fran Barrena <francisco@kyso.io>
2022-08-12 13:31:21 +0200

    fix: eslint

--- src/model/filesystem-item.model.ts
+++ src/model/filesystem-item.model.ts
@@ -1,8 +1,10 @@
-import { CreationReportFileSystemObject } from "./creation-report-file";
+import type { CreationReportFileSystemObject } from './creation-report-file';
 
 export class FilesystemItem {
   public file: CreationReportFileSystemObject;
+
   public children: FilesystemItem[];
+
   public level: number;
 
   constructor(file: CreationReportFileSystemObject, children: FilesystemItem[], level: number) {
@@ -12,14 +14,14 @@ export class FilesystemItem {
   }
 
   public hasChildren(): boolean {
-    return this.children && this.children.length > 0 ? true : false;
+    return !!(this.children && this.children.length > 0);
   }
 
   public static from(object: CreationReportFileSystemObject): FilesystemItem {
-    return new FilesystemItem(object, [], object.path.split("/").length);
+    return new FilesystemItem(object, [], object.path.split('/').length);
   }
-  
+
   public static fromArray(object: CreationReportFileSystemObject[]): FilesystemItem[] {
-    return object.map(x => FilesystemItem.from(x));
+    return object.map((x: CreationReportFileSystemObject) => FilesystemItem.from(x));
   }
 }
