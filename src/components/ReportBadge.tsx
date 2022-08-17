49ad0771a7e50842f1beef1296fb04837f80913b
Helena <helena@kyso.io>
2022-08-17 18:20:27 +0200

    fix: kyles hackathon bug reports' heading badge too small

--- src/components/ReportBadge.tsx
+++ src/components/ReportBadge.tsx
@@ -69,7 +69,7 @@ const ReportBadge = ({ report, authors, toggleUserStarReport, toggleUserPinRepor
         </div>
         <div className="flex-1 min-w-0 py-3 pr-2 relative">
           <a href={`/${report.organization_sluglified_name}/${report.team_sluglified_name}/${report.name}`} className="focus:outline-none">
-            <p className="text-sm font-medium text-gray-900 pb-2">{report.title}</p>
+            <h3 className="text-lg leading-6 font-medium text-gray-900">{report.title}</h3>
             <p className="text-sm text-gray-500">{description}</p>
           </a>
           <div className="absolute bottom-2 right-0">
