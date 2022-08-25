8fcef7b52b79d27eb54603b6fad57616899053be
Helena <helena@kyso.io>
2022-08-25 16:58:15 +0200

    fix: activity icons, tagged possition, thumbs up color, tags round border, channel name

--- src/components/InfoActivity.tsx
+++ src/components/InfoActivity.tsx
@@ -9,21 +9,21 @@ interface Props {
 const InfoActivity = ({ info }: Props) => {
   return (
     <div className="flex flex-row text-xs space-x-2">
-      <div className="flex items-center">
-        <DocumentReportIcon className="h-5 w-5 text-gray-500" />
+      <div className="flex items-center pr-4">
+        <DocumentReportIcon className="h-5 w-5 text-blue-500" />
         <span className="ml-1  text-gray-500">
           {info.reports} {info.reports === 1 ? 'report' : 'reports'}
         </span>
       </div>
-      <div className="flex items-center">
-        <ChatIcon className="h-5 w-5 text-gray-500" />
+      <div className="flex items-center pr-4">
+        <ChatIcon className="h-5 w-5 text-orange-500" />
         <span className="ml-1  text-gray-500">
           {info.comments} {info.comments === 1 ? 'comment' : 'comments'}
         </span>
       </div>
       {info.lastChange && (
-        <div className="flex items-center">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
+        <div className="flex items-center pr-4">
+          <CalendarIcon className="h-5 w-5 text-pink-500" />
           <span className="ml-1  text-gray-500">{moment(info.lastChange).format('DD/MM/YYYY')} last activity</span>
         </div>
       )}
