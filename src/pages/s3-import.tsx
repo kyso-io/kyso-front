454f5014550a3a4574a46df2992dc55e26d592e8
Helena <helena@kyso.io>
2022-09-07 12:57:29 +0200

    fix: improving metadata page

--- src/pages/s3-import.tsx
+++ src/pages/s3-import.tsx
@@ -6,6 +6,7 @@ import { useRedirectIfNoJWT } from '@/hooks/use-redirect-if-no-jwt';
 import MainLayout from '@/layouts/MainLayout';
 import type { CommonData } from '@/types/common-data';
 import { ArrowRightIcon } from '@heroicons/react/solid';
+import { Switch } from '@headlessui/react';
 // import type { NormalizedResponseDTO, Organization } from '@kyso-io/kyso-model';
 // import { Api } from '@kyso-io/kyso-store';
 // import { useRouter } from 'next/router';
@@ -23,15 +24,55 @@ const MetadataImport = ({ commonData }: Props) => {
   const [error, setError] = useState<string | null>(null);
   const [isBusy, setBusy] = useState(false);
 
-  const [s3, sets3] = useState<string>('');
+  const [awsKey, setAWSKey] = useState<string>('');
+  const [awsSecretKey, setAwsSecretKey] = useState<string>('');
+  const [s3Region, setS3Region] = useState<string>('');
+  const [s3Bucket, setS3Bucket] = useState<string>('');
+  const [s3Path, setS3Path] = useState<string>('');
+
+  const [author, setAuthor] = useState<string>('');
+  const [organization, setOrganization] = useState<string>('');
+  const [channel, setChannel] = useState<string>('');
+  const [defaulValue, setDefaulValue] = useState<boolean>(false);
 
   const importBucket = async (ev: any) => {
     ev.preventDefault();
     setError('');
-    if (!s3 || s3.length === 0) {
-      setError('Error.');
+    if (!awsKey || awsKey.length === 0) {
+      setError('Please specify a AWS Key.');
       return;
     }
+    if (!awsSecretKey || awsSecretKey.length === 0) {
+      setError('Please specify a AWS Secret Key.');
+      return;
+    }
+    if (!s3Region || s3Region.length === 0) {
+      setError('Please specify a S3 region.');
+      return;
+    }
+    if (!s3Bucket || s3Bucket.length === 0) {
+      setError('Please specify a S3 Bucket.');
+      return;
+    }
+    if (!s3Path || s3Path.length === 0) {
+      setError('Please specify a S3 Path.');
+      return;
+    }
+
+    // DEFAULT VALUE
+    if ((!author || author.length === 0) && !defaulValue) {
+      setError('Please specify an author.');
+      return;
+    }
+    if ((!organization || organization.length === 0) && !defaulValue) {
+      setError('Please specify an Organization.');
+      return;
+    }
+    if ((!channel || channel.length === 0) && !defaulValue) {
+      setError('Please specify an Channel.');
+      return;
+    }
+
     setBusy(true);
     try {
       // const api: Api = new Api(commonData.token);
@@ -80,13 +121,13 @@ const MetadataImport = ({ commonData }: Props) => {
                         <div className="max-w-lg flex rounded-md shadow-sm">
                           <input
                             type="text"
-                            name="s3"
-                            id="s3"
-                            value={s3}
-                            autoComplete="s3"
+                            name="awsKey"
+                            id="awsKey"
+                            value={awsKey}
+                            autoComplete="awsKey"
                             onChange={(e) => {
                               setError('');
-                              sets3(e.target.value);
+                              setAWSKey(e.target.value);
                             }}
                             className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
                           />
@@ -95,19 +136,19 @@ const MetadataImport = ({ commonData }: Props) => {
                     </div>
                     <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
                       <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                        AWS SECRET KEY
+                        AWS Secret Key
                       </label>
                       <div className="mt-1 sm:mt-0 sm:col-span-2">
                         <div className="max-w-lg flex rounded-md shadow-sm">
                           <input
                             type="password"
-                            name="s3"
-                            id="s3"
-                            value={s3}
-                            autoComplete="s3"
+                            name="awsSecretKey"
+                            id="awsSecretKey"
+                            value={awsSecretKey}
+                            autoComplete="awsSecretKey"
                             onChange={(e) => {
                               setError('');
-                              sets3(e.target.value);
+                              setAwsSecretKey(e.target.value);
                             }}
                             className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
                           />
@@ -116,19 +157,19 @@ const MetadataImport = ({ commonData }: Props) => {
                     </div>
                     <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
                       <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                        S3 REGION
+                        S3 Region
                       </label>
                       <div className="mt-1 sm:mt-0 sm:col-span-2">
                         <div className="max-w-lg flex rounded-md shadow-sm">
                           <input
                             type="text"
-                            name="s3"
-                            id="s3"
-                            value={s3}
-                            autoComplete="s3"
+                            name="s3Bucket"
+                            id="s3Bucket"
+                            value={s3Bucket}
+                            autoComplete="s3Bucket"
                             onChange={(e) => {
                               setError('');
-                              sets3(e.target.value);
+                              setS3Region(e.target.value);
                             }}
                             className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
                           />
@@ -137,19 +178,40 @@ const MetadataImport = ({ commonData }: Props) => {
                     </div>
                     <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
                       <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                        S3BUCKET
+                        S3 Bucket
                       </label>
                       <div className="mt-1 sm:mt-0 sm:col-span-2">
                         <div className="max-w-lg flex rounded-md shadow-sm">
                           <input
                             type="text"
-                            name="s3"
-                            id="s3"
-                            value={s3}
-                            autoComplete="s3"
+                            name="s3Bucket"
+                            id="s3Bucket"
+                            value={s3Bucket}
+                            autoComplete="s3Bucket"
                             onChange={(e) => {
                               setError('');
-                              sets3(e.target.value);
+                              setS3Bucket(e.target.value);
+                            }}
+                            className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+                      <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                        S3 Path
+                      </label>
+                      <div className="mt-1 sm:mt-0 sm:col-span-2">
+                        <div className="max-w-lg flex rounded-md shadow-sm">
+                          <input
+                            type="text"
+                            name="s3Path"
+                            id="s3Path"
+                            value={s3Path}
+                            autoComplete="s3Path"
+                            onChange={(e) => {
+                              setError('');
+                              setS3Path(e.target.value);
                             }}
                             className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
                           />
@@ -161,117 +223,117 @@ const MetadataImport = ({ commonData }: Props) => {
 
                 {/* Import data */}
                 <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
                     <div className="sm:col-span-3">
                       <h2 className="text-xl font-medium text-blue-gray-900">Import Data</h2>
                       <p className="mt-1 text-sm text-blue-gray-500">This information will be displayed </p>
                     </div>
                   </div>
+
                   <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                      S3PATH
+                    <label htmlFor="name" className={classNames(defaulValue ? 'text-gray-400' : 'text-gray-700', 'block text-sm font-medium sm:mt-px sm:pt-2')}>
+                      Organization
                     </label>
                     <div className="mt-1 sm:mt-0 sm:col-span-2">
                       <div className="max-w-lg flex rounded-md shadow-sm">
                         <input
                           type="text"
-                          name="s3"
-                          id="s3"
-                          value={s3}
-                          autoComplete="s3"
+                          name="organization"
+                          id="organization"
+                          value={organization}
+                          autoComplete="organization"
                           onChange={(e) => {
+                            if (defaulValue) {
+                              return;
+                            }
                             setError('');
-                            sets3(e.target.value);
+                            setOrganization(e.target.value);
                           }}
-                          className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
+                          className={classNames(
+                            defaulValue ? ' bg-gray-50 border-gray-500 text-gray-400 focus:ring-0 focus:border-gray-600 caret-gray-50' : ' focus:ring-indigo-500 focus:border-indigo-500',
+                            'flex-1 block w-full min-w-0 rounded-md sm:text-sm border-gray-300',
+                          )}
                         />
                       </div>
                     </div>
                   </div>
                   <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                      Default Author
+                    <label htmlFor="name" className={classNames(defaulValue ? 'text-gray-400' : 'text-gray-700', 'block text-sm font-medium sm:mt-px sm:pt-2')}>
+                      Channel
                     </label>
                     <div className="mt-1 sm:mt-0 sm:col-span-2">
                       <div className="max-w-lg flex rounded-md shadow-sm">
                         <input
                           type="text"
-                          name="s3"
-                          id="s3"
-                          value={s3}
-                          autoComplete="s3"
+                          name="channel"
+                          id="channel"
+                          value={channel}
+                          autoComplete="channel"
                           onChange={(e) => {
+                            if (defaulValue) {
+                              return;
+                            }
                             setError('');
-                            sets3(e.target.value);
+                            setChannel(e.target.value);
                           }}
-                          className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
+                          className={classNames(
+                            defaulValue ? ' bg-gray-50 border-gray-500 text-gray-400 focus:ring-0 focus:border-gray-600 caret-gray-50' : ' focus:ring-indigo-500 focus:border-indigo-500',
+                            'flex-1 block w-full min-w-0 rounded-md sm:text-sm border-gray-300',
+                          )}
                         />
                       </div>
                     </div>
                   </div>
                   <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                      Default Organization
+                    <label htmlFor="name" className={classNames(defaulValue ? 'text-gray-400' : 'text-gray-700', 'block text-sm font-medium sm:mt-px sm:pt-2')}>
+                      Author
                     </label>
                     <div className="mt-1 sm:mt-0 sm:col-span-2">
                       <div className="max-w-lg flex rounded-md shadow-sm">
                         <input
                           type="text"
-                          name="s3"
-                          id="s3"
-                          value={s3}
-                          autoComplete="s3"
+                          name="author"
+                          id="author"
+                          value={author}
+                          autoComplete="author"
                           onChange={(e) => {
+                            if (defaulValue) {
+                              return;
+                            }
                             setError('');
-                            sets3(e.target.value);
+                            setAuthor(e.target.value);
                           }}
-                          className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
+                          className={classNames(
+                            defaulValue ? ' bg-gray-50 border-gray-500 text-gray-400 focus:ring-0 focus:border-gray-600 caret-gray-50' : ' focus:ring-indigo-500 focus:border-indigo-500',
+                            'flex-1 block w-full min-w-0 rounded-md sm:text-sm border-gray-300',
+                          )}
                         />
                       </div>
                     </div>
                   </div>
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                      Default Channel
-                    </label>
-                    <div className="mt-1 sm:mt-0 sm:col-span-2">
-                      <div className="max-w-lg flex rounded-md shadow-sm">
-                        <input
-                          type="text"
-                          name="s3"
-                          id="s3"
-                          value={s3}
-                          autoComplete="s3"
-                          onChange={(e) => {
-                            setError('');
-                            sets3(e.target.value);
-                          }}
-                          className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
+                  <Switch.Group as="div" className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:pt-5">
+                    <Switch.Label as="dt" className={classNames(defaulValue ? 'text-gray-700' : 'text-gray-500', 'block text-sm font-medium sm:mt-px sm:pt-2')} passive>
+                      Automatic timezone
+                    </Switch.Label>
+                    <dd className="mt-1 text-gray-900 sm:col-span-2 sm:mt-0">
+                      <Switch
+                        checked={defaulValue}
+                        onChange={setDefaulValue}
+                        className={classNames(
+                          defaulValue ? 'bg-indigo-600' : 'bg-gray-200',
+                          'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 sm:ml-auto',
+                        )}
+                      >
+                        <span
+                          aria-hidden="true"
+                          className={classNames(
+                            defaulValue ? 'translate-x-5' : 'translate-x-0',
+                            'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+                          )}
                         />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
-                    <label htmlFor="name" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                      Use default data
-                    </label>
-                    <div className="mt-1 sm:mt-0 sm:col-span-2">
-                      <div className="max-w-lg flex rounded-md shadow-sm">
-                        <input
-                          type="text"
-                          name="s3"
-                          id="s3"
-                          value={s3}
-                          autoComplete="s3"
-                          onChange={(e) => {
-                            setError('');
-                            sets3(e.target.value);
-                          }}
-                          className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded-md sm:text-sm border-gray-300"
-                        />
-                      </div>
-                    </div>
-                  </div>
+                      </Switch>
+                    </dd>
+                  </Switch.Group>
                 </div>
               </div>
 
