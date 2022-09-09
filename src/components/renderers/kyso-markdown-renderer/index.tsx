78dc4a67357281744e814ba25812dacddac7efab
Fran Barrena <francisco@kyso.io>
2022-09-09 19:02:44 +0200

    fix: local images rendering in markdown

--- src/components/renderers/kyso-markdown-renderer/index.tsx
+++ src/components/renderers/kyso-markdown-renderer/index.tsx
@@ -11,6 +11,33 @@ interface Props {
   context?: ReportContext;
 }
 
+const translateImagesToSCSUrl = (source: string, c: ReportContext) => {
+  const results = source.match(imageRegex);
+  let finalContent = source;
+
+  if (results) {
+    for (const image of results) {
+      const imageUrl = image.match(extractFromParenthesis);
+      console.log(imageUrl);
+      if (imageUrl) {
+        let s1 = imageUrl.pop();
+        s1 = s1?.substring(1);
+        s1 = s1?.slice(0, -1);
+
+        const newUrl = `/scs/${c.organizationSlug}/${c.teamSlug}/reports/${c.reportSlug}/${c.version}/${s1}`;
+
+        const processedImage = image.replace(s1!, newUrl);
+
+        console.log(image);
+        console.log(processedImage);
+        finalContent = finalContent.replace(image, processedImage);
+      }
+    }
+  }
+
+  return finalContent;
+};
+
 export const RenderMarkdown = (props: Props) => {
   useNavigateToHashOnce({ active: true });
   let finalContent = null;
@@ -19,31 +46,7 @@ export const RenderMarkdown = (props: Props) => {
     finalContent = props.source.toString();
 
     if (props.context) {
-      const results = props.source.toString().match(imageRegex);
-
-      if (results) {
-        for (const image of results) {
-          const imageUrl = image.match(extractFromParenthesis);
-          console.log(imageUrl);
-          if (imageUrl) {
-            let s1 = imageUrl.pop();
-            s1 = s1?.substring(1);
-            s1 = s1?.slice(0, -1);
-
-            if (s1?.startsWith('.') || s1?.startsWith('/')) {
-              s1 = s1?.substring(1);
-            } else if (s1?.startsWith('./')) {
-              s1 = s1?.substring(2);
-            }
-
-            const newUrl = `/scs/${props.context?.organizationSlug}/${props.context?.teamSlug}/reports/${props.context?.reportSlug}/${props.context?.version}/${s1}`;
-
-            const processedImage = image.replace(s1!, newUrl);
-
-            finalContent = finalContent.replace(image, processedImage);
-          }
-        }
-      }
+      finalContent = translateImagesToSCSUrl(props.source.toString(), props.context);
     }
 
     return <MarkdownWrapper source={finalContent}></MarkdownWrapper>;
