bbd1f861e974c34e725b966eefd821b9bda03672
Fran Barrena <francisco@kyso.io>
2022-09-15 12:06:57 +0200

    fix: showing fulltextsearch results

--- src/components/search-item.tsx
+++ src/components/search-item.tsx
@@ -30,107 +30,126 @@ const SearchItem = ({ fullTextSearchResult, otherVersionResultsNumber }: Props) 
   };
 
   return (
-    <li className="mb-4 relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-      <div className="flex justify-between space-x-3">
-        <div className="min-w-0 flex-1">
-          <a href={`${basePath}${fullTextSearchResult.link}`} className="block focus:outline-none">
-            <span className="absolute inset-0" aria-hidden="true"></span>
-            {fullTextSearchResult.type !== ElasticSearchIndex.Comment && (
-              <>
-                <p className="text-sm mb-2 font-medium text-gray-900 truncate">
-                  {FileTypesHelper.isPowerpoint(fullTextSearchResult.filePath) ? (
-                    <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/powerpoint.svg`} alt="Powerpoint File" />
-                  ) : (
-                    ''
-                  )}
+    <>
+      <style>{`
+        em {
+          font-weight: bolder
+        }
+      `}</style>
 
-                  {FileTypesHelper.isWord(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/word.svg`} alt="Word File" /> : ''}
+      <li className="mb-4 relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+        <div className="flex justify-between space-x-3">
+          <div className="min-w-0 flex-1">
+            <a href={`${basePath}${fullTextSearchResult.link}`} className="block focus:outline-none">
+              <span className="absolute inset-0" aria-hidden="true"></span>
+              {fullTextSearchResult.type !== ElasticSearchIndex.Comment && (
+                <>
+                  <p className="text-sm mb-2 font-medium text-gray-900 truncate">
+                    {FileTypesHelper.isPowerpoint(fullTextSearchResult.filePath) ? (
+                      <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/powerpoint.svg`} alt="Powerpoint File" />
+                    ) : (
+                      ''
+                    )}
 
-                  {FileTypesHelper.isExcel(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/excel.svg`} alt="Excel File" /> : ''}
+                    {FileTypesHelper.isWord(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/word.svg`} alt="Word File" /> : ''}
 
-                  {FileTypesHelper.isPDF(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/pdf.svg`} alt="PDF File" /> : ''}
+                    {FileTypesHelper.isExcel(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/excel.svg`} alt="Excel File" /> : ''}
 
-                  {FileTypesHelper.isJupyterNotebook(fullTextSearchResult.filePath) ? (
-                    <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/jupyter.svg`} alt="Jupyter File" />
-                  ) : (
-                    ''
-                  )}
+                    {FileTypesHelper.isPDF(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/pdf.svg`} alt="PDF File" /> : ''}
 
-                  {FileTypesHelper.isMarkdown(fullTextSearchResult.filePath) ? (
-                    <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/markdown.svg`} alt="Markdown File" />
-                  ) : (
-                    ''
-                  )}
+                    {FileTypesHelper.isJupyterNotebook(fullTextSearchResult.filePath) ? (
+                      <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/jupyter.svg`} alt="Jupyter File" />
+                    ) : (
+                      ''
+                    )}
 
-                  {FileTypesHelper.isPython(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/python.svg`} alt="Python File" /> : ''}
+                    {FileTypesHelper.isMarkdown(fullTextSearchResult.filePath) ? (
+                      <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/markdown.svg`} alt="Markdown File" />
+                    ) : (
+                      ''
+                    )}
 
-                  {FileTypesHelper.isR(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/r.svg`} alt="R File" /> : ''}
+                    {FileTypesHelper.isPython(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/python.svg`} alt="Python File" /> : ''}
 
-                  {FileTypesHelper.isJavascript(fullTextSearchResult.filePath) ? (
-                    <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/javascript.svg`} alt="Javascript File" />
-                  ) : (
-                    ''
-                  )}
+                    {FileTypesHelper.isR(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/r.svg`} alt="R File" /> : ''}
 
-                  {FileTypesHelper.isTypescript(fullTextSearchResult.filePath) ? (
-                    <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/typescript.svg`} alt="Typescript File" />
-                  ) : (
-                    ''
-                  )}
+                    {FileTypesHelper.isJavascript(fullTextSearchResult.filePath) ? (
+                      <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/javascript.svg`} alt="Javascript File" />
+                    ) : (
+                      ''
+                    )}
 
-                  {FileTypesHelper.isHTML(fullTextSearchResult.filePath) ? (
-                    <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/${FileTypesHelper.getBrowser()}`} alt="HTML File" />
-                  ) : (
-                    ''
-                  )}
+                    {FileTypesHelper.isTypescript(fullTextSearchResult.filePath) ? (
+                      <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/typescript.svg`} alt="Typescript File" />
+                    ) : (
+                      ''
+                    )}
 
-                  {isUnknownFile(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/file.svg`} alt="Typescript File" /> : ''}
+                    {FileTypesHelper.isHTML(fullTextSearchResult.filePath) ? (
+                      <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/${FileTypesHelper.getBrowser()}`} alt="HTML File" />
+                    ) : (
+                      ''
+                    )}
 
-                  {fullTextSearchResult.title}
+                    {isUnknownFile(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/file.svg`} alt="Typescript File" /> : ''}
 
-                  {otherVersionResultsNumber && otherVersionResultsNumber > 1 && (
-                    <span className="text-xs font-semibold inline-block py-1 px-2 ml-2 rounded text-black bg-white last:mr-0 mr-1 border">{`+${otherVersionResultsNumber - 1} more versions`}</span>
-                  )}
+                    {fullTextSearchResult.title}
 
-                  {
-                    <div style={{ marginLeft: '27px', fontSize: '0.9em', fontFamily: 'monospace' }}>
-                      {fullTextSearchResult.filePath.replace(
-                        `/${fullTextSearchResult.organizationSlug}/${fullTextSearchResult.teamSlug}/reports/${Helper.slugify(fullTextSearchResult.title)}/${fullTextSearchResult.version}`,
-                        '.',
-                      )}
-                    </div>
-                  }
-                </p>
-              </>
-            )}
-            {fullTextSearchResult.organizationSlug && (
-              <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{fullTextSearchResult.organizationSlug}</span>
-            )}
-            {fullTextSearchResult.teamSlug && (
-              <span className="bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{fullTextSearchResult.teamSlug}</span>
-            )}
-            {fullTextSearchResult.people &&
-              Array.isArray(fullTextSearchResult.people) &&
-              fullTextSearchResult.people.map((person: string, index: number) => (
-                <span key={index} className="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">
-                  {person}
-                </span>
-              ))}
-            {fullTextSearchResult.tags &&
-              Array.isArray(fullTextSearchResult.tags) &&
-              fullTextSearchResult.tags.map((tag: string, index: number) => (
-                <span key={index} className="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">
-                  {tag}
-                </span>
-              ))}
-          </a>
+                    {otherVersionResultsNumber && otherVersionResultsNumber > 1 && (
+                      <span className="text-xs font-semibold inline-block py-1 px-2 ml-2 rounded text-black bg-white last:mr-0 mr-1 border">{`+${otherVersionResultsNumber - 1} more versions`}</span>
+                    )}
+
+                    {
+                      <div style={{ marginLeft: '27px', fontSize: '0.9em', fontFamily: 'monospace' }}>
+                        {fullTextSearchResult.filePath.replace(
+                          `/${fullTextSearchResult.organizationSlug}/${fullTextSearchResult.teamSlug}/reports/${Helper.slugify(fullTextSearchResult.title)}/${fullTextSearchResult.version}`,
+                          '.',
+                        )}
+                      </div>
+                    }
+                  </p>
+                </>
+              )}
+
+              {fullTextSearchResult.type === ElasticSearchIndex.Comment && (
+                <>
+                  <p className="text-sm mb-2 font-medium text-gray-900 truncate">
+                    {isUnknownFile(fullTextSearchResult.filePath) ? <img className="h-5 w-5 mr-2" style={{ display: 'inline' }} src={`/assets/images/file.svg`} alt="Typescript File" /> : ''}
+
+                    {fullTextSearchResult.link.split('/').pop()?.replaceAll('-', ' ')}
+                  </p>
+                </>
+              )}
+
+              {fullTextSearchResult.organizationSlug && (
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{fullTextSearchResult.organizationSlug}</span>
+              )}
+              {fullTextSearchResult.teamSlug && (
+                <span className="bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{fullTextSearchResult.teamSlug}</span>
+              )}
+              {fullTextSearchResult.people &&
+                Array.isArray(fullTextSearchResult.people) &&
+                fullTextSearchResult.people.map((person: string, index: number) => (
+                  <span key={index} className="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">
+                    {person}
+                  </span>
+                ))}
+              {fullTextSearchResult.tags &&
+                Array.isArray(fullTextSearchResult.tags) &&
+                fullTextSearchResult.tags.map((tag: string, index: number) => (
+                  <span key={index} className="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">
+                    {tag}
+                  </span>
+                ))}
+            </a>
+          </div>
         </div>
-      </div>
-      {/* Content show */}
-      {/* <div className="mt-2">
-        <p className="line-clamp-2 text-sm text-gray-600">{fullTextSearchResult.content}</p>
-      </div> */}
-    </li>
+        {/* Content show */}
+        <div className="mt-2">
+          <p className="line-clamp-2 text-sm text-gray-600" dangerouslySetInnerHTML={{ __html: fullTextSearchResult.content }}></p>
+        </div>
+      </li>
+    </>
   );
 };
 
