f110676bf69ea2751196e1dbe8369d7e72d7ec29
Daniel Andújar <dani@kyso.io>
2022-10-25 14:09:21 +0200

    fix: check if a file is main, hide the option in the editor

--- src/components/FilesystemEntry.tsx
+++ src/components/FilesystemEntry.tsx
@@ -138,7 +138,7 @@ const FilesystemEntry = (props: FilesystemEntryProps) => {
                   </>
                 )}
 
-                {fileType !== 'folder' && (
+                {fileType !== 'folder' && !item.file.main && (
                   <button
                     className={classNames('w-full hover:cursor-pointer text-gray-700 hover:bg-gray-100', 'group flex items-center px-4 py-2 text-sm text-gray-400 group-hover:text-gray-500')}
                     onClick={() => {
