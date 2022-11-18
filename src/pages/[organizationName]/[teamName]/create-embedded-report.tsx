71320fd39c5b24163ad46603dfb77cd881b47097
Daniel Andújar <daniandujar@gmail.com>
2022-11-18 15:33:34 +0100

    fix: add cancel button in create-embedded-report page

--- src/pages/[organizationName]/[teamName]/create-embedded-report.tsx
+++ src/pages/[organizationName]/[teamName]/create-embedded-report.tsx
@@ -563,6 +563,22 @@ const CreateEmbeddedReport = ({ commonData, setUser }: Props) => {
               <div className="flex flex-row items-center space-x-2">
                 <div className="mr-2 mt-2">
                   <PureKysoButton
+                    type={KysoButton.SECONDARY}
+                    onClick={() => {
+                      let redirectUrl = `/${router.query.organizationName}`;
+                      if (router.query.teamName) {
+                        redirectUrl += `/${router.query.teamName}`;
+                      }
+                      router.replace(redirectUrl);
+                    }}
+                  >
+                    <div className="flex flex-row items-center">Cancel</div>
+                  </PureKysoButton>
+                </div>
+              </div>
+              <div className="flex flex-row items-center space-x-2">
+                <div className="mr-2 mt-2">
+                  <PureKysoButton
                     type={!hasPermissionCreateReport ? KysoButton.PRIMARY_DISABLED : KysoButton.PRIMARY}
                     disabled={!hasPermissionCreateReport || busy}
                     onClick={() => createReport()}
