815b8bf5ceb6e9e01d567574fcdaf605ce6af0f1
Fran Barrena <francisco@kyso.io>
2022-11-23 18:42:43 +0100

    feat: trying to avoid important

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,13 @@ import '../styles/globals.css';
 import '../styles/styles.css';
 
 import type { ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 import { store } from '@kyso-io/kyso-store';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { Helper } from '@/helpers/Helper';
+import type { KeyValue } from '@/model/key-value.model';
+import { KysoSettingsEnum } from '@kyso-io/kyso-model';
 import type { PageWithLayoutType } from '../types/pageWithLayout';
 
 type AppLayoutProps = AppProps & {
@@ -12,14 +16,31 @@ type AppLayoutProps = AppProps & {
   /* eslint-disable @typescript-eslint/no-explicit-any */
   pageProps: any;
 };
+
 function MyApp({ Component, pageProps }: AppLayoutProps) {
+  const [theme, setTheme] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getTheme = async () => {
+      const publicKeys: KeyValue[] = await Helper.getKysoPublicSettings();
+      const keyValue: KeyValue | undefined = publicKeys.find((x) => x.key === KysoSettingsEnum.THEME);
+      if (keyValue && keyValue.value) {
+        setTheme(keyValue.value);
+      }
+    };
+    getTheme();
+  }, []);
+
   const Layout = Component.layout || ((children: ReactNode) => <>{children}</>);
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+      {theme && <link rel="stylesheet" href={`/pub/themes/${theme}/styles.css`}></link>}
+    </>
   );
 }
 export default MyApp;
