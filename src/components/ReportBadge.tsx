ca10ab81ad60b5f7cdc4f0c002181873c9861754
Fran Barrena <francisco@kyso.io>
2022-12-22 12:00:14 +0100

    fix: solved responsive

--- src/components/ReportBadge.tsx
+++ src/components/ReportBadge.tsx
@@ -73,7 +73,7 @@ const ReportBadge = ({ commonData, report, authors, toggleUserStarReport, toggle
             <h3 className="text-sm lg:text-lg leading-6 font-medium text-gray-900 break-words">{report.title}</h3>
             <p className="text-xs lg:text-sm text-gray-500 pt-3 break-words">{description}</p>
           </a>
-          <div className="hidden lg:visible absolute bottom-2 right-0">
+          <div className="hidden lg:block absolute bottom-2 right-0">
             {report.report_type && (
               <span className="bg-orange-100 text-orange-800 text-xs font-semibold mr-2 px-2.5 py-1 rounded-xl dark:bg-orange-200 dark:text-orange-900">{report.report_type}</span>
             )}
