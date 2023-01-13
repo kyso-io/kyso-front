f2e21d9ae4d48611386a362dd3aad11575ca96aa
Fran Barrena <francisco@kyso.io>
2023-01-13 13:09:45 +0100

    feat: added better error page and a koala

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -27,13 +27,67 @@ class ErrorBoundary extends React.Component {
     if ((this.state as any).hasError) {
       // You can render any custom fallback UI
       return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-
-          <button type="button" onClick={() => this.setState({ hasError: false })}>
-            Try again?
-          </button>
-        </div>
+        <>
+          {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full">
+          <body class="h-full">
+          ```
+        */}
+          <div
+            className="flex flex-col bg-white lg:relative"
+            style={{
+              height: '1000px',
+              marginTop: '10em',
+            }}
+          >
+            <div className="flex grow flex-col">
+              <main className="flex grow flex-col bg-white">
+                <div className="mx-auto flex w-full max-w-7xl grow flex-col px-6 lg:px-8">
+                  <div className="shrink-0 pt-10 sm:pt-16">
+                    <a href="/" className="inline-flex">
+                      <span className="sr-only">Your Company</span>
+                      <img className="h-12 mr-2" style={{ display: 'inline' }} src={`/assets/images/kyso-logo-and-name-dark.svg`} alt="Kyso Logo" />
+                    </a>
+                  </div>
+                  <div className="my-auto shrink-0 py-16 sm:py-32">
+                    <p className="text-base font-semibold text-indigo-600"></p>
+                    <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Unexpected error</h1>
+                    <p className="mt-2 text-base text-gray-500">Sorry, we ran in an unexpected error, we will solve it as soon as possible.</p>
+                    <div className="mt-6">
+                      <a href="#" className="text-base font-medium text-indigo-600 hover:text-indigo-500">
+                        Go back home
+                        <span aria-hidden="true"> &rarr;</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </main>
+              <footer className="shrink-0 bg-gray-50">
+                <div className="mx-auto w-full max-w-7xl py-16 px-6 lg:px-8">
+                  <nav className="flex space-x-4">
+                    <a href="/feedback" className="text-sm font-medium text-gray-500 hover:text-gray-600">
+                      Contact Support
+                    </a>
+                    {/* <span className="inline-block border-l border-gray-300" aria-hidden="true" />
+                  <a href="#" className="text-sm font-medium text-gray-500 hover:text-gray-600">
+                    Status
+                  </a>
+                  <span className="inline-block border-l border-gray-300" aria-hidden="true" />
+                  <a href="#" className="text-sm font-medium text-gray-500 hover:text-gray-600">
+                    Twitter
+      </a> */}
+                  </nav>
+                </div>
+              </footer>
+            </div>
+            <div className="hidden lg:absolute lg:inset-y-0 lg:right-0 lg:block lg:w-1/2">
+              <img className="absolute inset-0 h-full w-full object-cover" src={`/assets/images/ai_koala.jpg`} alt="" />
+            </div>
+          </div>
+        </>
       );
     }
 
