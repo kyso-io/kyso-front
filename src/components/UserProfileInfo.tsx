7738949156e6d6d10f15687da7417d0081b2fd44
Fran Barrena <francisco@kyso.io>
2023-01-21 18:22:00 +0100

    fix: hovering image report badge to edit

--- src/components/UserProfileInfo.tsx
+++ src/components/UserProfileInfo.tsx
@@ -5,7 +5,6 @@ import { TailwindHeightSizeEnum } from '@/tailwind/enum/tailwind-height.enum';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { faPeriod } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { PlusSmIcon as PlusSmIconSolid } from '@heroicons/react/solid';
 import type { UserDTO } from '@kyso-io/kyso-model';
 import moment from 'moment';
 import { useRouter } from 'next/router';
@@ -60,7 +59,9 @@ const UserProfileInfo = (props: IUserProfileInfo) => {
                 className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-gray-700 bg-white hover:bg-gray-50  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 title="Change background image"
               >
-                <PlusSmIconSolid className="h-5 w-5" aria-hidden="true" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                  <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+                </svg>
               </button>
               <input
                 ref={imageInputFileRef}
