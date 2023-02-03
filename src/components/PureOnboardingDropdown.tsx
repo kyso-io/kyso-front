984f184ba1392219d6b609eae11eae5753bcd790
Fran Barrena <francisco@kyso.io>
2023-02-03 16:27:31 +0100

    fix: changed onboarding icon

--- src/components/PureOnboardingDropdown.tsx
+++ src/components/PureOnboardingDropdown.tsx
@@ -1,4 +1,3 @@
-import { BellIcon } from '@heroicons/react/solid';
 import { Menu, Transition } from '@headlessui/react';
 import React, { useState, Fragment, useEffect } from 'react';
 import { useUser } from '@/hooks/use-user';
@@ -174,14 +173,20 @@ const PureOnboardingDropdown = () => {
     <Menu as="div" className="relative">
       <div>
         <Menu.Button
-          className="flex max-w-xs items-center rounded-full text-sml-auto bg-white p-1 text-green-500 ver:text-indigo-500/75 focus:outline-none ring-2 ring-green-500 focus:ring-offset-2 focus:ring-offset-kyso-600"
+          className="flex max-w-xs items-center text-sml-auto p-1 text-white ver:text-indigo-500/75 focus:outline-none focus:ring-offset-2 focus:ring-offset-kyso-600"
           onClick={() => {
             setOpen(!open);
             setValue('');
           }}
         >
           <span className="sr-only">View notifications</span>
-          <BellIcon className="h-6 w-6" aria-hidden="true" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+            />
+          </svg>
         </Menu.Button>
       </div>
       <div>
