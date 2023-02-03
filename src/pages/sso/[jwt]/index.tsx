50108cbb0226d122f53e2f47f87a6ef7307c94bf
Fran Barrena <francisco@kyso.io>
2023-02-03 16:23:46 +0100

    fix: redirection to overview for sso login

--- src/pages/sso/[jwt]/index.tsx
+++ src/pages/sso/[jwt]/index.tsx
@@ -1,6 +1,8 @@
 import NoLayout from '@/layouts/NoLayout';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import decode from 'jwt-decode';
+import type { DecodedToken } from '@/types/decoded-token';
 
 const Index = () => {
   const router = useRouter();
@@ -9,9 +11,14 @@ const Index = () => {
   useEffect(() => {
     if (jwt) {
       localStorage.setItem('jwt', jwt as string);
+      const jwtToken: DecodedToken = decode<DecodedToken>(jwt as string);
 
       setTimeout(() => {
-        router.push('/');
+        if (jwtToken.payload.show_onboarding) {
+          router.push('/overview');
+        } else {
+          router.push('/');
+        }
       }, 200);
     }
   }, [jwt]);
