6b2609013f92886034e9ab56a6c501e10fdb472a
Daniel Andújar <daniandujar@gmail.com>
2023-03-28 13:56:42 +0200

    feat: give tailwind style to table from jupyter notebook

--- src/components/renderers/kyso-jupyter-renderer/components/output-cell-text-html.tsx
+++ src/components/renderers/kyso-jupyter-renderer/components/output-cell-text-html.tsx
@@ -1,11 +1,47 @@
-import { useRef, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useMemo, useRef, useState } from 'react';
 
 interface Props {
   outputs: string[];
 }
 
 const OutputCellHtml = ({ outputs }: Props) => {
-  const plainHtml: string = outputs.join('');
+  const plainHtml: string = useMemo(() => {
+    const htmlOutputs: string = outputs.join('');
+    if (htmlOutputs.indexOf('<table') > -1) {
+      const indexStartTable: number = outputs.findIndex((output: string) => output.indexOf('<table') > -1) + 1;
+      const indexEndTable: number = outputs.findIndex((output: string) => output.indexOf('</table>') > -1) - 1;
+      const tableContent: string = outputs.slice(indexStartTable, indexEndTable + 1).join('');
+      const table = document.createElement('table');
+      table.innerHTML = tableContent;
+      let html = `<div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">`;
+      html += `<div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">`;
+      html += `<table class="min-w-full divide-y divide-gray-300">`;
+      html += `<thead>`;
+      table.querySelectorAll('thead tr').forEach((row: any) => {
+        html += `<tr>`;
+        row.querySelectorAll('th').forEach((cell: any, index: number) => {
+          html += `<th scope="col" class="py-3.5 ${index === 0 ? 'pl-4 pr-3' : 'px-3'} text-left text-sm font-semibold text-gray-900">${cell.innerHTML}</th>`;
+        });
+        html += `</tr>`;
+      });
+      html += `</thead>`;
+      html += `<tbody class="divide-y divide-gray-200">`;
+      table.querySelectorAll('tbody tr').forEach((row: any) => {
+        html += `<tr>`;
+        row.querySelectorAll('th, td').forEach((cell: any, index: number) => {
+          html += `<td class="whitespace-nowrap py-4 ${index === 0 ? 'pl-4 pr-3' : 'px-3'} text-sm font-medium text-gray-500 sm:pl-0">${cell.innerHTML}</td>`;
+        });
+        html += `</tr>`;
+      });
+      html += `</tbody>`;
+      html += `</table>`;
+      html += `</div>`;
+      html += `</div>`;
+      return html;
+    }
+    return htmlOutputs;
+  }, [outputs]);
 
   /* eslint-disable @typescript-eslint/no-explicit-any */
   const ref = useRef<any>(null);
