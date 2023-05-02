4835ca1574c44ef516f78bc0aebace8ddc89b672
Fran Barrena <francisco@kyso.io>
2023-05-02 12:26:49 +0200

    fix: aria hidden to specific items

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -102,15 +102,15 @@ const Index = ({ commonData }: IKysoApplicationLayoutProps) => {
                 <a
                   href="/create-organization"
                   className="text-gray-500 hover:bg-gray-50 hover:text-gray-900 flex items-center px-3 py-2 text-xs lg:text-sm rounded-md"
-                  role="none"
+                  role="link"
                   style={{
                     float: 'right',
                     position: 'relative',
                     bottom: '50px',
                   }}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" className="w-5 h-5 mr-1" role="none">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" role="none"></path>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" className="w-5 h-5 mr-1" role="img">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" role="img"></path>
                   </svg>
                   Create
                 </a>
