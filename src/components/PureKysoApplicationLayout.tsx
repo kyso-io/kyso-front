2921d401d2809cd27d69a523b66f91952f273c13
Fran Barrena <francisco@kyso.io>
2023-05-26 18:13:28 +0200

    refactor: changed a for Link in avatars and users menu and breadcrumb

--- src/components/PureKysoApplicationLayout.tsx
+++ src/components/PureKysoApplicationLayout.tsx
@@ -219,13 +219,13 @@ const PureKysoApplicationLayout = (props: IPureKysoApplicationLayoutProps): Reac
                                           );
                                         }
                                         return (
-                                          <a
-                                            href={item.href}
+                                          <Link
+                                            href={item.href!}
                                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                             target={classNames(item.newTab ? '_blank' : '')}
                                           >
                                             {item.name}
-                                          </a>
+                                          </Link>
                                         );
                                       }}
                                     </Menu.Item>
