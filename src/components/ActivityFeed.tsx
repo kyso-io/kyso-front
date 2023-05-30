44cef37e2e48aca8b90d76d0683c502e550b7871
Fran Barrena <francisco@kyso.io>
2023-05-30 12:50:24 +0200

    fix: changed task icons

--- src/components/ActivityFeed.tsx
+++ src/components/ActivityFeed.tsx
@@ -6,6 +6,7 @@ import { ActionEnum, EntityEnum } from '@kyso-io/kyso-model';
 import moment from 'moment';
 import React, { useMemo } from 'react';
 import Link from 'next/link';
+import KTasksIcon from '@/icons/KTasksIcon';
 import PureAvatar from './PureAvatar';
 import TagInlineComment from './inline-comments/components/tag-inline-comment';
 
@@ -127,13 +128,7 @@ const ActivityFeedInlineComment = ({ activityFeed, relations }: ActivityFeedProp
           </div>
         </span>
         <span className="absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-400">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
-            />
-          </svg>
+          <KTasksIcon className="w-5 h-5 text-gray-400"></KTasksIcon>
         </span>
       </div>
       <div className="min-w-0 flex-1">
